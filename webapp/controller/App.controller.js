a60b4c4ad8a37da8f64ca7f2b0210a573a97847b
Mauricio Alcala <mauricio.alcala@softtek.com>
2019-05-15 17:52:14 +0000

    end_vide_2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,18 +1,31 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/base/Log"
+], function (Controller, Log) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
 		onInit: function () {
-
+			Log.info(" Controller inicializated: ---------------");
 		},
-		onPress: function(oEvent)
+		onBeforeRendering: function()
 		{
-			window.console.log(" OK");
+			Log.info(" The app will be render --------------");
+		},
+		onAfterRendering:  function()
+		{
+			Log.info(" The app has been rendered --------------");
+		},
+		onPress: function(myString, oEvent)
+		{
+			Log.info(" Mesaje de prueba ----------------------------");	
 			sap.ui.require(["sap/m/MessageToast"], function(oMessage, nOptions){
-				oMessage.show(" Searching movie...");
+				oMessage.show(" Searching movie..." + myString);
 			});
+		},
+		onExit: function()
+		{
+			Log.info(" Controller will shortly be destroyed --------------");
 		}
 	});
 });
