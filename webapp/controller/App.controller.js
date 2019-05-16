47b34d74e9ad038dca3c1ab8e2a038df862301c1
Mauricio Alcala <mauricio.alcala@softtek.com>
2019-05-16 15:32:07 +0000

    working

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/base/Log"
-], function (Controller, Log) {
+	"sap/base/Log",
+	"sap/ui//model/json/JSONModel",
+	"../model/formatter" ,
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Log, JSONModel, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("opensap.movies.controller.App", {
+		formatter: formatter,
 		onInit: function () {
 			Log.info(" Controller inicializated: ---------------");
 		},
@@ -16,16 +21,40 @@ sap.ui.define([
 		{
 			Log.info(" The app has been rendered --------------");
 		},
-		onPress: function(myString, oEvent)
-		{
-			Log.info(" Mesaje de prueba ----------------------------");	
-			sap.ui.require(["sap/m/MessageToast"], function(oMessage, nOptions){
-				oMessage.show(" Searching movie..." + myString);
+		onPress: function (sValue) {
+			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+			  var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			  oMessage.show(oResourceBundle.getText("search") + sValue);
+			}.bind(this));
+			
+			var sCity = this.byId('city').getValue(),
+			  sGenre = this.byId('genre').getSelectedItem().getKey(),
+			  oCalendar = this.byId("calendar"),
+			  oRowBinding = oCalendar.getBinding("rows"),
+			  oFilterGenre,
+			  oFilterCity;
+			
+			// Create filters for genre and city according to user inputs
+			oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+			oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+			
+			// Apply genre filter to calendar rows
+			oRowBinding.filter(oFilterGenre);
+			
+			// Apply city filter to row appointments
+			var aRows = oCalendar.getAggregation("rows");
+			aRows.forEach(function (oItem) {
+			  var oAppointmentsBinding = oItem.getBinding("appointments");
+			  oAppointmentsBinding.filter(oFilterCity);
 			});
 		},
 		onExit: function()
 		{
 			Log.info(" Controller will shortly be destroyed --------------");
+		},
+		onHello: function()
+		{
+			sap.m.MessageToast.show('Do you like to go to the movies?');
 		}
 	});
 });
